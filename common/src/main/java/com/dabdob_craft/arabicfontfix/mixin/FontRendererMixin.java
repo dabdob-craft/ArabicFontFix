c7340023c474cd2fc81cdfd62f2029623ef8dc40
dabdob-craft <dabdobcraft2@gmail.com>
2025-11-06 16:06:41 +0100

    Add mixin to fix Arabic text rendering
    Inject Arabic text fixing into the TextRenderer.

---
+++ common/src/main/java/com/dabdob_craft/arabicfontfix/mixin/FontRendererMixin.java
@@ -0,0 +1,21 @@
+package com.dabdob_craft.arabicfontfix.mixin;
+
+import com.dabdob_craft.arabicfontfix.ArabicFontFixer;
+import net.minecraft.client.font.TextRenderer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+
+@Mixin(TextRenderer.class)
+public class FontRendererMixin {
+
+    @ModifyVariable(
+        method = "drawInternal(Ljava/lang/String;FFIZLorg/joml/Matrix4f;Lnet/minecraft/client/render/RenderLayer;ZIZ)F",
+        at = @At("HEAD"), 
+        argsOnly = true, 
+        index = 1
+    )
+    private String injectArabicFix(String text) {
+        return ArabicFontFixer.fixText(text, true); 
+    }
+}
