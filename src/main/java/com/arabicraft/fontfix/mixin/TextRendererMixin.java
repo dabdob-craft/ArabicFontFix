e9c7c226675d74bed03e10be93b36fb6b1bc85f0
dabdob-craft <dabdobcraft2@gmail.com>
2025-12-20 21:05:14 +0100

    Test: Fast Holy-Renderer compatible build

--- src/main/java/com/arabicraft/fontfix/mixin/TextRendererMixin.java
+++ src/main/java/com/arabicraft/fontfix/mixin/TextRendererMixin.java
@@ -8,8 +8,8 @@ import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 @Mixin(TextRenderer.class)
 public class TextRendererMixin {
-    @ModifyVariable(method = "draw(Ljava/lang/String;FFIZLnet/minecraft/util/math/Matrix4f;Lnet/minecraft/client/render/VertexConsumerProvider;ZII)I", at = @At("HEAD"), argsOnly = true, remap = false)
+    @ModifyVariable(method = "draw(Ljava/lang/String;FFIZLnet/minecraft/util/math/Matrix4f;Lnet/minecraft/client/render/VertexConsumerProvider;ZII)I", at = @At("HEAD"), argsOnly = true)
     private String onDraw(String text) {
-        return ArabicReshaper.reshape(text);
+        return (text == null || text.isEmpty()) ? text : ArabicReshaper.reshape(text);
     }
 }
