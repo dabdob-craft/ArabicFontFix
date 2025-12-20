e70c462c33f63f46f2ada844248b0e8037c2b8fb
dabdob-craft <dabdobcraft2@gmail.com>
2025-12-20 14:09:46 +0100

    Update Quilt entrypoints and final class rename

--- src/main/java/com/arabicraft/fontfix/ArabicFontFixClient.java
+++ src/main/java/com/arabicraft/fontfix/ArabicFontFixClient.java
@@ -1,16 +1,12 @@
 package com.arabicraft.fontfix;
-
 import net.fabricmc.api.ClientModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-public class FabricFontFixClient implements ClientModInitializer {
-    
+public class ArabicFontFixClient implements ClientModInitializer {
     public static final String MOD_ID = "arabicfontfix";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-
     @Override
     public void onInitializeClient() {
-        LOGGER.info("Arabic Font Fix Mod Initializing... By Arabicraft Team!");
+        LOGGER.info("Arabic Font Fix Initialized!");
     }
 }
