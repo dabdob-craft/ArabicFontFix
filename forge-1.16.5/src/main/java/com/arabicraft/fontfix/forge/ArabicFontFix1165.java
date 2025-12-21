01cc24b48e5e1c84549a8dc503c4d75f15ca9639
dabdob-craft <dabdobcraft2@gmail.com>
2025-12-21 14:15:40 +0100

    add mods.toml

--- forge-1.16.5/src/main/java/com/arabicraft/fontfix/forge/ArabicFontFix1165.java
+++ forge-1.16.5/src/main/java/com/arabicraft/fontfix/forge/ArabicFontFix1165.java
@@ -1,15 +1,11 @@
 package com.arabicraft.fontfix.forge;
 
-import com.arabicraft.fontfix.core.ArabicReshaper;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.client.event.RenderGameOverlayEvent;
 
 @Mod("arabicfontfix")
 public class ArabicFontFix1165 {
     public ArabicFontFix1165() {
-        System.out.println("Arabic Font Fix for 1.16.5 Forge is Loading...");
         MinecraftForge.EVENT_BUS.register(this);
     }
 }
